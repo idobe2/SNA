65615ed89035c7d73dd12d61250509fcc39415bb
Ido <idobenoun@gmail.com>
2024-02-18 21:02:26 +0200

    get user most common

--- src/api/get_user_most_common_programming_language.py
+++ src/api/get_user_most_common_programming_language.py
@@ -1,6 +1,5 @@
 import csv
 import requests
-import time
 from collections import Counter
 from concurrent.futures import ThreadPoolExecutor
 
@@ -9,25 +8,14 @@ ACCESS_TOKEN = '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'
 
 
 def get_user_repositories(username):
-    """
-    Fetches the repositories of a given user from the GitHub API.
-
-    Args:
-        username (str): The GitHub username.
-
-    Returns:
-        list: List of URLs to the languages of the user's repositories.
-    """
     url = f"https://api.github.com/users/{username}/repos"
     headers = {
         "Authorization": f"token {ACCESS_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
     }
-    print(f"Fetching repositories for {username}...")
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         repos_data = response.json()
-        time.sleep(0.8)
         return [repo['languages_url'] for repo in repos_data]
     else:
         print(f"Failed to fetch repositories for {username}")
@@ -35,43 +23,19 @@ def get_user_repositories(username):
 
 
 def get_most_common_language(languages):
-    """
-    Finds the most common programming language from a list of languages.
-
-    Args:
-        languages (list): List of programming languages.
-
-    Returns:
-        str: The most common programming language.
-    """
     language_counter = Counter(languages)
     return language_counter.most_common(1)[0][0] if language_counter else 'No languages detected'
 
 
 def process_user(row):
-    """
-    Processes a user from the input CSV file.
-
-    Args:
-        row (dict): A dictionary containing user data.
-
-    Returns:
-        dict or None: Processed user data with the most common language added, or None if user processing was skipped.
-    """
     if row['bio'] != 'Bio not available':
         username = row['name']
         repos_languages = []
         for repo_languages_url in get_user_repositories(username):
             response = requests.get(repo_languages_url, headers={"Authorization": f"token {ACCESS_TOKEN}"})
-            # print(f"Request made: {response.request.method} {response.request.url}")
             if response.status_code == 200:
                 repo_languages_data = response.json()
                 repos_languages.extend(list(repo_languages_data.keys()))
-                # Introduce a delay to stay within rate limit
-                # 5000 requests per hour ~ 1.3 requests per second ~ 0.8 ms to wait * 10 threads
-                time.sleep(0.8)  # Adjust this delay as needed
-            else:
-                print(f"Failed to fetch repositories for {username}")
         most_common_language = get_most_common_language(repos_languages)
         row['most_common_language'] = most_common_language
         print(f"Fetched data for {username}: Most common language: {most_common_language}")
@@ -93,7 +57,7 @@ def main():
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        with ThreadPoolExecutor(max_workers=10) as executor:
+        with ThreadPoolExecutor(max_workers=5) as executor:
             results = executor.map(process_user, reader)
             for result in results:
                 if result:
