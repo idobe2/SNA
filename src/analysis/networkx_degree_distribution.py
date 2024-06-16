3aafe210686fdbb0526f9b1089feffb2a884e889
Ido <idobenoun@gmail.com>
2024-06-16 17:06:55 +0300

    Final update

--- src/analysis/networkx_degree_distribution.py
+++ src/analysis/networkx_degree_distribution.py
@@ -2,6 +2,7 @@
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Importing dataset
 users = pd.read_csv('../../csv/musae_git_target.csv')
@@ -15,13 +16,24 @@ followers = nx.parse_edgelist(Data, delimiter=',', create_using=nx.Graph(), node
 ml_target_dict = users.set_index('id')['ml_target'].to_dict()
 nx.set_node_attributes(followers, ml_target_dict, 'ml_target')
 
+# Function to calculate degree distribution with beta adjustment
+def degree_distribution(graph, beta):
+    degrees = [degree for node, degree in graph.degree()]
+    hist, bin_edges = np.histogram(degrees, bins=np.arange(0, max(degrees) + 2))
+    return hist ** beta
+
+# Control the beta value
+beta = 1.0  # You can change this value to see the effect
+
 # Calculate the degree distribution for each group
 degrees_ml_0 = [degree for node, degree in followers.degree() if followers.nodes[node]['ml_target'] == 0]
 degrees_ml_1 = [degree for node, degree in followers.degree() if followers.nodes[node]['ml_target'] == 1]
 
-# Create a histogram of the degree distribution
-degree_distribution_ml_0 = nx.degree_histogram(followers.subgraph([node for node in followers if followers.nodes[node]['ml_target'] == 0]))
-degree_distribution_ml_1 = nx.degree_histogram(followers.subgraph([node for node in followers if followers.nodes[node]['ml_target'] == 1]))
+# Create a histogram of the degree distribution with beta adjustment
+degree_distribution_ml_0 = degree_distribution(
+    followers.subgraph([node for node in followers if followers.nodes[node]['ml_target'] == 0]), beta)
+degree_distribution_ml_1 = degree_distribution(
+    followers.subgraph([node for node in followers if followers.nodes[node]['ml_target'] == 1]), beta)
 
 # Plot the degree distribution for ml_target = 0 in log-log scale
 fig, ax = plt.subplots(figsize=(15, 10))
@@ -46,4 +58,3 @@ ax.set_xlabel('Degree')
 ax.set_ylabel('Frequency')
 ax.set_yscale('log')
 plt.show()
-
